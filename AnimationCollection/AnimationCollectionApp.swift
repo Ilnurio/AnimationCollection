669e19a8c27562c4d48da2bcb410839933d31e39
Ilnur Fattakhov <nurich1995@mail.ru>
2023-07-07 18:36:07 +0300

    Initial Commit

---
+++ AnimationCollection/AnimationCollectionApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimationCollectionApp.swift
+//  AnimationCollection
+//
+//  Created by Ilnur on 07.07.2023.
+//
+
+import SwiftUI
+
+@main
+struct AnimationCollectionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
